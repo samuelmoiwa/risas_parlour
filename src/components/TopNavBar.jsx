99b871f70cc3af9b051ca37c3c6079a0b4a81db7
samuelmoiwa <samuelmoiwa@gmail.com>
2023-08-29 16:56:01 +0000

    update navBar.jsx

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const TopNavBar = () => {
   return (
-    <div className='h-32 bg-yellow-300'>
+    <div className='h-32 bg-yellow-300 flex  flex-col items-center'>
+        <div className='bg-red-500 w-10/12'>
+        <div>
+            welcome to Risa's Palour
+        </div>
+        </div>
         TopNavBar
     </div>
   )
