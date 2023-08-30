ac2411792caabb354118ff1e77b4e8e9a649a3c8
samuelmoiwa <samuelmoiwa@gmail.com>
2023-08-30 16:54:25 +0000

    update hero banner

--- src/components/HeroBannerCarousel.jsx
+++ src/components/HeroBannerCarousel.jsx
@@ -1,30 +1,11 @@
+import React, { Link } from "react";
 import { useState, useEffect } from "react";
-import { Carousel, IconButton } from "@material-tailwind/react";
+import { Carousel, IconButton, Typography } from "@material-tailwind/react";
+import apple_watch_1 from '../images/apple_watch_1.png'
+import gray_1 from '../images/gray_1.jpg'
 
 export function HeroBannerCarousel() {
   const [activeIndex, setActiveIndex] = useState("");
-
-  const slides = [
-    { color: "bg-red-600" },
-    { color: "bg-blue-600" },
-    { color: "bg-green-600" },
-    { color: "bg-red-600" },
-    { color: "bg-blue-600" },
-    { color: "bg-green-600" },
-    // Add more slide elements here
-  ];
-
-  const autoChangeSlide = () => {
-    console.log("Changing slide");
-    setActiveIndex((prevIndex) => (prevIndex + 1) % slides.length);
-  };
-
-
-  useEffect(() => {
-    const interval = setInterval(autoChangeSlide, 5000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <Carousel
       className="flex flex-row w-full bg-pink-500"
@@ -43,9 +24,70 @@ export function HeroBannerCarousel() {
         </div>
       )}
     >
-      {slides.map((slide, index) => (
-        <div key={index} className={`w-screen h-full overflow-x-hidden ${slide.color}`} />
-      ))}
+
+
+
+      <div className="w-screen h-full overflow-x-hidden bg-white
+            bg-cover bg-center bg-no-repeat items-center justify-center
+      "
+        style={{ backgroundImage: `url(${gray_1})` }}
+      >
+        <div
+          className="
+          flex flex-col justify-center p-6
+          lg:py-24 lg:px-14 lg:flex-row sm:h-96
+          lg:justify-between
+          xl:ml-5"
+        >
+
+        <div
+          className="
+          container flex flex-col justify-center p-6
+          sm:py-16 lg:py-24 lg:px-14 lg:flex-row
+          lg:justify-between pb-12 pt-20 bg-green-500
+          xl:ml-5  "
+        >
+          <div
+            className="
+            flex flex-col justify-center
+            text-center rounded-sm
+            lg:max-w-xl xl:max-w-2xl lg:text-left"
+          >
+            <Typography
+              variant="lead"
+              color="white"
+              className="text-gray-600"
+            >
+              <span className="font-bold text-4xl text-gray-700">Apple Watch</span> <br/>
+              Apple Watch Series 3 Apple Watch Series 2 MacBook Pro, apple watch series 3,
+              gadget, electronics, mobile Phone png
+            </Typography>
+
+            </div>
+        </div>
+
+        <div
+          className="
+          flex items-center justify-center p-6 mt-2
+          lg:max-w-3xl xl:max-w-3xl bg-pink-500
+          lg:w-2/4
+          "
+        >
+          <img
+            src={apple_watch_1}
+            alt="Service"
+            className="object-contain object-center h-full w-full"
+          />
+        </div>
+
+      </div>
+
+
+      </div>
+      <div className="w-screen h-full overflow-x-hidden bg-red-500">
+      </div>
+      <div className="w-screen h-full overflow-x-hidden bg-red-600">
+      </div>
     </Carousel>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { HeroBannerCarousel } from '../components/HeroBannerCarousel';
 const Home = () => {
   return (
     <>
-    <div className='h-100 w-full'>
+    <div className='h-96 w-full'>
       < HeroBannerCarousel />
     </div>
       <div className='text-gray-500 font-bold bg-gray-100 mt-10 flex justify-center py-10'> Home Page</div>
