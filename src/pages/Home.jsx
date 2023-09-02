0c7b2bd1c75415c8774fbee09b188f87f4a694e6
samuelmoiwa <samuelmoiwa@gmail.com>
2023-09-02 15:47:12 +0000

    update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import { HeroBannerCarousel } from '../components/HeroBannerCarousel';
 const Home = () => {
   return (
     <>
-    <div className='h-96 w-full'>
+
       < HeroBannerCarousel />
-    </div>
+    
       <div className='text-gray-500 font-bold bg-gray-100 mt-10 flex justify-center py-10'> Home Page</div>
     </>
 
